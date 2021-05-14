2a6433d3bd5095de830afbee6b1d1347a7474389
amanrsayyad <79994006+amanrsayyad@users.noreply.github.com>
2021-05-14 00:16:02 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const  menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+
+// let stars = document.getElementById('stars');
+// let stars2 = document.getElementById('stars2');
+// let moon = document.getElementById('moon');
+// let rock = document.getElementById('rock');
+// let masjid = document.getElementById('masjid');
+// let text = document.getElementById('text');
+// let header = document.getElementById('header');
+
+
+// window.addEventListener('scroll', function(){
+//     let value = window.scrollY;
+//     stars.style.bottom = value * 0.05 + '%';
+//     stars2.style.bottom = value * 0.025 + '%';
+//     moon.style.bottom = value * -0.5 + 'px';
+//     rock.style.bottom = value * -0.75 + 'px';
+//     masjid.style.bottom = value * -0.15 + 'px';
+
+
+//     text.style.right = -100 + value * 0.5 + '%';
+//     text.style.top = 50 + value * 0.1 + '%';
+
+//     // header.style.top = value * 0.5 + 'px';
+// })
+
+
+
+
+
+ 
